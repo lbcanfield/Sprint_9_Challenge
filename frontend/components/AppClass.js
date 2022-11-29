9c9f51156cc36906636b68f208310cb13a8ec3d1
lbcanfield <lewie.b.canfield@gmail.com>
2022-11-29 13:25:50 -0600

    cleaning up for mvp tests

--- frontend/components/AppClass.js
+++ frontend/components/AppClass.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import axios from 'axios'
 
 import Keypad from './classComponents/Keypad';
 import SubmitForm from './classComponents/SubmitForm';
@@ -102,9 +103,35 @@ export default class AppClass extends React.Component {
     }
 
   }
+  onChange = (event) => {
+    // You will need this to update the value of the input.
+    event.preventDefault();
+    this.setState({ ...this.state, email: event.target.value })
+  }
 
 
+  onSubmit = (event) => {
+    // Use a POST request to send a payload to the server.
+    event.preventDefault();
+    const newPlayer = {
+      'x': this.getXY()[0],
+      'y': this.getXY()[1],
+      'steps': this.state.steps,
+      'email': this.state.email
+    }
 
+    console.log(newPlayer);
+    axios.post('http://localhost:9000/api/result', newPlayer)
+      .then(response => {
+        // console.log(response)
+        this.setState({ ...this.state, winningMsg: (response.data.message) });
+      })
+      .catch(error => {
+        this.setState({ ...this.state, winningMsg: (error.response.data.message) });
+      })
+    this.setState({ ...this.state, email: '' })
+
+  }
 
   render() {
     console.log(this.state)
@@ -120,7 +147,15 @@ export default class AppClass extends React.Component {
           <h3 id="message">{this.state.message}</h3>
         </div>
         <Keypad reset={this.reset} getNextIndex={this.getNextIndex} />
-        <SubmitForm winningMsg={this.state.winningMsg} getXY={this.getXY} steps={this.state.steps} />
+
+
+        <div>
+          <form onSubmit={this.onSubmit}>
+            <input onChange={this.onChange} value={this.state.email} id="email" type="email" placeholder="type email"></input>
+            <input id="submit" type="submit"></input>
+          </form>
+          <div>{this.state.winningMsg}</div>
+        </div>
       </div>
     )
   }
